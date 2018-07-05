bcdb119e9e6334a1926590bedd6cbd01bd5a1c74
bdebetu <40872876+bdebetu@users.noreply.github.com>
2018-07-06 00:07:13 +0200

    Add files via upload

---
+++ teeload/config.php
@@ -0,0 +1,9 @@
+<?php
+
+ /* An XML database can easily be read, therefore, private information should be set here */
+	
+	$user = "Betu"; // username
+	$passw = "admin"; // password
+	$apikey = "AD999F09A81362627D85347D8790161D"; // Find your API Key at https://steamcommunity.com/dev/apikey
+
+?>
